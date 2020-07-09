e29a9931c5288fd60b6a290c11f2ac75fc93e818
Rowan Cannaday <cannadayr@gmail.com>
2020-07-08 20:18:22 -0400

    add pometo as a dependency

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rebar3 compile && erl -pa _build/default/lib/svc/ebin/ -eval 'application:start(svc)'
+rebar3 compile && erl -pa _build/default/lib/svc/ebin/ -pa _build/default/lib/rebar/ebin/ -eval 'application:start(svc)'
 
